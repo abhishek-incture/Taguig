f123707c0646606fcce8863744e79431a96be5d1
Abhishek Kumar <abhishek.kumar@incture.com>
2020-03-30 15:12:40 +0530

    Toolbar and Bottom Navigation

--- app/src/main/java/com/incture/taguig/LoginActivity.java
+++ app/src/main/java/com/incture/taguig/LoginActivity.java
@@ -5,18 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginButton);
+
+
+        loginButton.setOnClickListener(this);
     }
 
     public void goToCreateAccount(View view) {
 
         Intent intent = new Intent(this,RegisterActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.loginButton:
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
