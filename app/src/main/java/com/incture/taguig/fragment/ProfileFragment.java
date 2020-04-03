032bb603fc484b73f80ca2546290eb8e1c5a754c
Abhishek Kumar <abhishek.kumar@incture.com>
2020-04-03 10:35:33 +0530

    Profile Screen-3

--- app/src/main/java/com/incture/taguig/fragment/ProfileFragment.java
+++ app/src/main/java/com/incture/taguig/fragment/ProfileFragment.java
@@ -1,36 +1,36 @@
 package com.incture.taguig.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.incture.taguig.R;
-import com.incture.taguig.adapter.BroadcastAdapter;
 import com.incture.taguig.adapter.FriendsListAdapter;
-import com.incture.taguig.utils.AutoFitGridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     RecyclerView friendsListRecycler;
     FriendsListAdapter friendsListAdapter;
 
     Map<String, String> friendsListMap;
     ArrayList<Map> arrayList = new ArrayList<>();
+
+    TextView friendsButton, pollsButton, surveysButton;
+    LinearLayout surveysLinear, pollsLinear;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,6 +40,11 @@ public class ProfileFragment extends Fragment {
 
 
         friendsListRecycler = (RecyclerView) view.findViewById(R.id.friendsListRecycler);
+        friendsButton = (TextView) view.findViewById(R.id.friendsButton);
+        pollsButton = (TextView) view.findViewById(R.id.pollsButton);
+        surveysButton = (TextView) view.findViewById(R.id.surveysButton);
+        surveysLinear = (LinearLayout) view.findViewById(R.id.surveysLinear);
+        pollsLinear = (LinearLayout) view.findViewById(R.id.pollsLinear);
 
         for(int i=0; i<=20; i++){
             friendsListMap = new HashMap<>();
@@ -53,10 +58,74 @@ public class ProfileFragment extends Fragment {
         friendsListRecycler.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         friendsListRecycler.setAdapter(friendsListAdapter);
 
-
+        friendsButton.setOnClickListener(this);
+        pollsButton.setOnClickListener(this);
+        surveysButton.setOnClickListener(this);
 
 
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.friendsButton:
+                friendsButton.setBackgroundResource(R.drawable.bg_buttongrey);
+                friendsButton.setTextColor(getActivity().getResources().getColor(R.color.colorDarkRed));
+                friendsButton.setTextSize(TypedValue.COMPLEX_UNIT_SP,14);
+
+                pollsButton.setTextColor(getActivity().getResources().getColor(R.color.colorDarkGrey));
+                pollsButton.setTextSize(TypedValue.COMPLEX_UNIT_SP,13);
+                pollsButton.setBackgroundColor(getActivity().getResources().getColor(R.color.buttongrey));
+
+                surveysButton.setBackgroundColor(getActivity().getResources().getColor(R.color.buttongrey));
+                surveysButton.setTextColor(getActivity().getResources().getColor(R.color.colorDarkGrey));
+                surveysButton.setTextSize(TypedValue.COMPLEX_UNIT_SP,13);
+
+                friendsListRecycler.setVisibility(View.VISIBLE);
+                pollsLinear.setVisibility(View.GONE);
+                surveysLinear.setVisibility(View.GONE);
+
+                break;
+
+            case R.id.pollsButton:
+                pollsButton.setBackgroundResource(R.drawable.bg_buttongrey);
+                pollsButton.setTextColor(getActivity().getResources().getColor(R.color.colorDarkRed));
+                pollsButton.setTextSize(TypedValue.COMPLEX_UNIT_SP,14);
+
+                friendsButton.setTextColor(getActivity().getResources().getColor(R.color.colorDarkGrey));
+                friendsButton.setTextSize(TypedValue.COMPLEX_UNIT_SP,13);
+                friendsButton.setBackgroundColor(getActivity().getResources().getColor(R.color.buttongrey));
+
+                surveysButton.setBackgroundColor(getActivity().getResources().getColor(R.color.buttongrey));
+                surveysButton.setTextColor(getActivity().getResources().getColor(R.color.colorDarkGrey));
+                surveysButton.setTextSize(TypedValue.COMPLEX_UNIT_SP,13);
+
+                friendsListRecycler.setVisibility(View.GONE);
+                pollsLinear.setVisibility(View.VISIBLE);
+                surveysLinear.setVisibility(View.GONE);
+
+                break;
+
+            case R.id.surveysButton:
+                surveysButton.setBackgroundResource(R.drawable.bg_buttongrey);
+                surveysButton.setTextColor(getActivity().getResources().getColor(R.color.colorDarkRed));
+                surveysButton.setTextSize(TypedValue.COMPLEX_UNIT_SP,14);
+
+                friendsButton.setBackgroundColor(getActivity().getResources().getColor(R.color.tvdarkGrey));
+                friendsButton.setTextColor(getActivity().getResources().getColor(R.color.colorDarkGrey));
+                friendsButton.setTextSize(TypedValue.COMPLEX_UNIT_SP,13);
+
+
+                pollsButton.setBackgroundColor(getActivity().getResources().getColor(R.color.tvdarkGrey));
+                pollsButton.setTextColor(getActivity().getResources().getColor(R.color.colorDarkGrey));
+                pollsButton.setTextSize(TypedValue.COMPLEX_UNIT_SP,13);
+
+                friendsListRecycler.setVisibility(View.GONE);
+                pollsLinear.setVisibility(View.GONE);
+                surveysLinear.setVisibility(View.VISIBLE);
+
+                break;
+        }
+    }
 }
